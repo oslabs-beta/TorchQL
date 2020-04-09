70e5b93b59c89f396bc07d9cd4558ff71c95e5f3
Zac H <zac.haluza@gmail.com>
2020-04-09 17:37:40 -0400

    working webpack config

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  devServer: {
+    historyApiFallback: true,
+    // include proxy settings once we've incorporated a back end
+    // proxy: {
+    //   '/': 'http://localhost:3000',
+    // },
+  },
+  entry: path.resolve(__dirname, 'client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'static/media/[name].[hash:8].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'client/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+};
