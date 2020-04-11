66f8273eb7042d747f0cb870cbc51b0bf817fa8f
Sara Chang <sarachang530@gmail.com>
2020-04-11 17:47:35 -0400

    worked on Input.jsx

---
+++ client/Components/Input.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={(e) => this.props.handleInput(e)}>Submit</button>
+        <input
+          value={this.props.URI}
+          onChange={(e) => this.props.handleURI(e)}
+        />
+      </div>
+    );
+  }
+}
+
+export default Input;
