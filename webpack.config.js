9352d46b9d9a0f544f6926f8e98efea6dd38ac87
Jimmy Chen <jimchn@jimmys-mbp.lan>
2020-04-11 16:22:04 -0400

    added electron to our project

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     // },
   },
   entry: path.resolve(__dirname, 'client/index.js'),
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
