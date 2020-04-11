28bcc99f2e49239be911f9e1b3b0076b0e26c2e7
Eric Tang <jivesushi@yahoo.com>
2020-04-11 19:25:53 -0400

    placed helper functions in separate file

---
+++ server/functions/helperFunctions.js
@@ -0,0 +1,53 @@
+
+
+function capitalize(str) {
+    return `${str[0].toUpperCase()}${str.slice(1)}`;
+  }
+  
+function typeSet(str) {
+	switch (str) {
+		case "character varying":
+			return "String";
+			break;
+		case "character":
+			return "String";
+			break;
+		case "integer":
+			return "Int";
+			break;
+		case "text":
+			return "String";
+			break;
+		case "date":
+			return "String";
+			break;
+		case "boolean":
+			return "Boolean";
+			break;
+		default: 
+			return "Int";
+	}
+}
+  
+// supposed to check for one-to-many relationship between foreign key and primary key on two tables, doesn't work yet
+function refsMany({ table, tableKey, ref, refKey }) {
+//   const queryStr = `SELECT * FROM people INNER JOIN planets ON planets._id = people.homeworld_id`;
+//   console.log('querystr: ', queryStr);
+//   db.query(queryStr, (err, data) => {
+//     if (err) {
+//       return err;
+//     } else {
+//         console.log('data.rows :', data.rows);
+//         console.log('data.rows.length :', data.rows.length);
+//         if (data.rows.length > 1) { 
+//           return true
+//         } else return false;
+//       }
+//   });
+	return false;
+}
+
+module.exports = {
+	capitalize,
+	typeSet
+};
--- server/functions/typesCreator.js
+++ server/functions/typesCreator.js
@@ -1,51 +1,5 @@
 const { singular } = require("pluralize");
-
-function capitalize(str) {
-  return `${str[0].toUpperCase()}${str.slice(1)}`;
-}
-
-function typeSet(str) {
-  switch (str) {
-    case "character varying":
-      return "String";
-      break;
-    case "character":
-      return "String";
-      break;
-    case "integer":
-      return "Int";
-      break;
-    case "text":
-      return "String";
-      break;
-    case "date":
-      return "String";
-      break;
-    case "boolean":
-      return "Boolean";
-      break;
-    default: 
-      return "Int";
-  }
-}
-
-// supposed to check for one-to-many relationship between foreign key and primary key on two tables, doesn't work yet
-function refsMany({ table, tableKey, ref, refKey }) {
-//   const queryStr = `SELECT * FROM people INNER JOIN planets ON planets._id = people.homeworld_id`;
-//   console.log('querystr: ', queryStr);
-//   db.query(queryStr, (err, data) => {
-//     if (err) {
-//       return err;
-//     } else {
-//         console.log('data.rows :', data.rows);
-//         console.log('data.rows.length :', data.rows.length);
-//         if (data.rows.length > 1) { 
-//           return true
-//         } else return false;
-//       }
-//   });
-  return false;
-}
+const { capitalize, typeSet } = require('./helperFunctions');
 
 // returns query types in SDL as string
 function createQuery(arr) {
@@ -102,7 +56,7 @@ function createMutation(arr) {
 
 // returns custom objects types in SDL as string
 function createTypes(arr) {
-  let typeStr = '';
+	let typeStr = '';
   for({ tableName, primaryKey, foreignKeys, columns } of arr) {
     const fkCache = {};
     for (let key of foreignKeys) fkCache[key.name] = key;
@@ -113,8 +67,8 @@ function createTypes(arr) {
       if (fkCache[column.columnName]) {
         const { name, referenceTable, referenceKey } = fkCache[column.columnName];
         // supposed to check for one-to-many relationship before displaying type as array
-        if (refsMany(fkCache[column.columnName])) typeStr += `\n  ${name}:[${capitalize(referenceTable)}]`;
-        else typeStr += `\n  ${name}:${capitalize(singular(referenceTable))}`;
+        // if (refsMany(fkCache[column.columnName])) typeStr += `\n  ${name}:[${capitalize(referenceTable)}]`;
+        typeStr += `\n  ${name}:${capitalize(singular(referenceTable))}`;
       // adds remaining columns with types to SDL string
       } else if (column.columnName !== primaryKey) {
         typeStr += `\n  ${column.columnName}:${typeSet(column.dataType)}`;
