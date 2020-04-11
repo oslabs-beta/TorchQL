551de7ab2bb66ee8c27f9727b60d503086ec2725
Eric Tang <jivesushi@yahoo.com>
2020-04-10 21:35:15 -0400

    first commit

--- server/routes/database.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const dbController = require('./../controllers/dbController');
-
-router.get('/pg',
-  dbController.getPGTables,
-  (req, res) => res.status(200).json(res.locals.tables)
-);
-
-module.exports = router;
