331ec7243f1837c977cbb396b1f67c259ff9ee95
jimchn <jimmchen12249@gmail.com>
2020-04-11 23:32:14 -0400

    added a menu for electron

--- server/main.js
+++ server/main.js
@@ -1,11 +1,13 @@
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, Menu} = electron
 const path = require('path');
 const url = require('url');
 
 
 
 /* Electron logic */
+
+let mainWindow
 
 // Checks/Listen for the app to be ready to use
 app.on('ready', () => {
@@ -18,7 +20,37 @@ app.on('ready', () => {
     protocol:'file',
     slashes: true
   }))
+
+  // Quit App when closed
+  mainWindow.on('closed', () => {
+    app.quit()
+  })
+
+  // Build Menu
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+
+  // Insert Menu
+  Menu.setApplicationMenu(mainMenu)
 })
+
+// Create Menu Template
+const mainMenuTemplate = [
+  {
+      label: 'File',
+      submenu:[
+          {
+              label: 'Placeholder'
+          },
+          {
+              label: 'Quit',
+              accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+              click(){
+                  app.quit() // closes the program/gooey
+              }
+          }
+      ]
+  }
+] 
 
 
 
