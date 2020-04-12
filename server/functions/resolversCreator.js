16ca864e640fdb07b6552e0c356b0286fba08ab5
Eric Tang <jivesushi@yahoo.com>
2020-04-12 09:54:47 -0400

    fixed spacing in formatting of query all and byId resolvers

--- server/functions/resolversCreator.js
+++ server/functions/resolversCreator.js
@@ -3,14 +3,15 @@ const { singular } = require("pluralize");
 const generateGetAllQuery = (arr) => {
     const queriesAll = [];
     for ({ tableName } of arr) {
-      let resolveStr = `${tableName}: () => {
-          try{
-              const query = 'SELECT * FROM ${tableName}';
-              return db.query(query).then((res) => res.rows)
-          } catch (err) {
-              throw new Error(err);
-          }
+      let resolveStr = 
+      `${tableName}: () => {
+      try{
+        const query = 'SELECT * FROM ${tableName}';
+        return db.query(query).then((res) => res.rows)
+      } catch (err) {
+        throw new Error(err);
       }
+    }
       `;
     queriesAll.push(resolveStr);
     }
@@ -21,13 +22,13 @@ const generateGetAllQuery = (arr) => {
     const queriesById = [];
     for ({ tableName, primaryKey } of arr) {
     let resolveStr = `${singular(tableName)}ById: (parent, args) => {
-        try{
-            const query = 'SELECT * FROM ${tableName} WHERE ${primaryKey} = $1';
-            const values = [args.${primaryKey}]
-            return db.query(query).then((res) => res.rows)
-        } catch (err) {
-            throw new Error(err);
-        }
+      try{
+        const query = 'SELECT * FROM ${tableName} WHERE ${primaryKey} = $1';
+        const values = [args.${primaryKey}]
+        return db.query(query).then((res) => res.rows)
+      } catch (err) {
+        throw new Error(err);
+      }
     }
     `;
     queriesById.push(resolveStr);
@@ -36,8 +37,7 @@ const generateGetAllQuery = (arr) => {
   };
 
   const generateQueryResolvers = (arr1, arr2) => {
-    return `const resolvers = {\nQuery: {
-        ${arr1.join('\n')}\n${arr2.join('\n')}
+    return `const resolvers = {\n\xa0\xa0Query: {\n\xa0\xa0\xa0\xa0${arr1.join('\n\xa0\xa0\xa0\xa0')}\n\xa0\xa0\xa0\xa0${arr2.join('\n\xa0\xa0\xa0\xa0')}
     }`;
   };
 
