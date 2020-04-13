bbda1a16c6617eb13b1e392c54fefd30a7e05b1a
Sara Chang <sarachang530@gmail.com>
2020-04-13 18:21:14 -0400

    added scss color shortcuts

--- client/Components/Input.jsx
+++ client/Components/Input.jsx
@@ -4,12 +4,16 @@ import React, { Component } from 'react';
 class Input extends Component {
   render() {
     return (
-      <div>
-        <button onClick={(e) => this.props.handleInput(e)}>Submit</button>
+      <div className="Form">
+        <h1 className="piQL">PiQL</h1>
         <input
+          className="Input"
           value={this.props.URI}
           onChange={(e) => this.props.handleURI(e.target.value)}
         />
+        <button className="MainBtn" onClick={(e) => this.props.handleInput(e)}>
+          Submit
+        </button>
       </div>
     );
   }
