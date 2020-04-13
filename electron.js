7e4d03bd08effae023143492b3f3bd1fe7ec7b8f
Zac H <zac.haluza@gmail.com>
2020-04-12 20:03:27 -0400

    moved to root, renamed, adjusted pathname

---
+++ electron.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+const path = require('path');
+const url = require('url');
+
+/* Electron logic */
+
+// Checks/Listen for the app to be ready to use
+app.on('ready', () => {
+  // Creates a new window
+  mainWindow = new BrowserWindow({});
+
+  // Loads the html into the Electron App
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, './dist/index.html'),
+      protocol: 'file',
+      slashes: true,
+    })
+  );
+});
