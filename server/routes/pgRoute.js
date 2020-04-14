740ff942bc8323a577f2f3a4d08d4c3d275b2afd
Eric Tang <jivesushi@yahoo.com>
2020-04-14 11:23:06 -0400

    sending res.locals.schema back from get request

--- server/routes/pgRoute.js
+++ server/routes/pgRoute.js
@@ -14,7 +14,7 @@ router.get(
   pgController.assembleSchema,
   (req, res) => {
     console.log(res.locals.schema);
-    res.status(200).json(res.locals.tables);
+    res.status(200).json(res.locals.schema);
   }
 );
 
