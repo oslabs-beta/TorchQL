553998d7b0d4d888020c91fe22de4f22ad9f6c96
Sara Chang <sarachang530@gmail.com>
2020-04-15 15:59:05 -0400

    query generator syntax corrections

--- server/programmatically-definedSchemas/generators/queryGenerator.js
+++ server/programmatically-definedSchemas/generators/queryGenerator.js
@@ -3,11 +3,11 @@ const { capitalize } = require('../helpers/helperFunctions');
 
 const QueryGenerator = {};
 
-QueryGenerator.allColumns = table => {
-    const singularName = singular(table);
-    capSingle = capitalize(singularName)
 
-    // RootQuery output to get all tables
+// RootQuery output to get all tables
+QueryGenerator.allColumns = table => {
+  const singularName = singular(table);
+  const capSingle = capitalize(singularName)
     return `    ${table}: {\n`
     +`    type: new GraphQLList(${capSingle}Type),\n`
     +`      resolve() { \n`
@@ -19,13 +19,14 @@ QueryGenerator.allColumns = table => {
     +`        },\n`
     +`      }\n`
     +`    },\n`  
-}
+};
 
+
+
+// RootQuery output to get single table by id
 QueryGenerator.column = table => {
-    const singlularName = singular(table);
-    capSingle = capitalize(singularName);
-
-    // RootQuery output to get single table by id
+  const singlularName = singular(table);
+  const capSingle = capitalize(singularName);
     return `${singlularName}ByID: {`
     +`    type: ${capSingle}Type,\n`
     +`    args: {\n`
