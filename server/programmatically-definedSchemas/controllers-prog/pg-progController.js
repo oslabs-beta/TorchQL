f2f661ed2838dc00fe6d0ca6c88df10e66483f96
jimchn <jimmychen12249@gmail.com>
2020-04-15 16:11:31 -0400

    deleted function folders

--- server/programmatically-definedSchemas/controllers-prog/pg-progController.js
+++ server/programmatically-definedSchemas/controllers-prog/pg-progController.js
@@ -1,22 +1,4 @@
-// Root Query 
-let rootQuery =
-`const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      // GET all users
-      ${tableName}: {
-        type: new GraphQLList(${singular(tableName)}Type),
-        resolve() {
-          try {
-            const query = 'SELECT * FROM ${tableName}';
-            return db.query(query).then((res) => res.rows);
-          } catch (err) {
-            throw new Error(err);
-          }
-        },
-      },
-    }
-});`
-function getAllQuery(data) {
-    
-}
+const fs = require('fs');
+const { Pool } = require('pg');
+const pgQuery = fs.readFileSync('server/queries/tableData.sql', 'utf8');
+const pgController = {};
