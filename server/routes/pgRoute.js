4817ffce8e9656b2f9c7544bb7a81b01a9f92831
Zac H <zac.haluza@gmail.com>
2020-04-15 23:46:09 -0400

    added mutation route and middleware

--- server/routes/pgRoute.js
+++ server/routes/pgRoute.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const pgController = require('./../controllers/pgController');
 
-router.get('/pg-sdl',
+router.get(
+  '/pg-sdl',
   pgController.getPGTables,
   pgController.makeQueries,
   pgController.makeMutations,
@@ -17,11 +18,16 @@ router.get('/pg-sdl',
   }
 );
 
-router.get('/pg-prog',
-  
+router.get(
+  '/pg-prog',
+
+  pgController.getPGTables,
+  pgController.generateMutations,
+  pgController.assembleMutations,
+  pgController.formatMutations,
   (req, res) => {
-    console.log(res.locals.schema);
-    res.status(200).json(res.locals.schema);
-)
+    res.status(200).json(res.locals.mutations);
+  }
+);
 
 module.exports = router;
