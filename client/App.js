c1d1062f3635ef654151656937b58c0b5c31b700
Sara Chang <sarachang530@gmail.com>
2020-04-17 19:08:22 -0400

    started refactoring react class to hooks

--- client/App.js
+++ client/App.js
@@ -1,58 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import MainDisplay from './components/MainDisplay';
+
 
 import './styles.scss';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      URI: '',
-      schema: [],
-      displayCode: false,
-      searchHistory: [],
-    };
-    this.handleInput = this.handleInput.bind(this);
-    this.handleURI = this.handleURI.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.addToSearchHistory = this.addToSearchHistory.bind(this);
-  }
+
+
+const App = () => {
+   const [searchHistory, setSearchHistory] = useState([]);
+  const [URI, setURI] = useState('');
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       URI: '',
+//       schema: [],
+//       displayCode: false,
+//       searchHistory: [],
+//     };
+//     this.handleInput = this.handleInput.bind(this);
+//     this.handleURI = this.handleURI.bind(this);
+//     this.handleClick = this.handleClick.bind(this);
+//     this.addToSearchHistory = this.addToSearchHistory.bind(this);
+//   }
 
   // Add inputted URI to search history
-  addToSearchHistory() {
-    this.setState(
-      { searchHistory: [...this.state.searchHistory, this.state.URI] },
-      () => {
-        this.setState({ URI: '' });
-      }
-    );
+  const addToSearchHistory = () => {
+    setSearchHistory([...searchHistory, URI]);
+    setURI('');
   }
 
-  handleInput(event) {
-    event.preventDefault();
-    if (this.state.URI !== '') {
-      fetch(`/db/pg?uri=${this.state.URI}`)
-        .then((data) => data.json())
-        .then((data) => {
-          this.addToSearchHistory();
-          this.setState({ schema: data, displayCode: true });
-          console.log('this.state.schema: ', this.state.schema);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
-  }
-  handleURI(input) {
-    this.setState({ URI: input });
-  }
-  // for CodeDisplay.jsx/Back button
-  handleClick(event) {
-    event.preventDefault();
-    this.setState({ displayCode: false });
-  }
-
-  render() {
+  // handleInput(event) {
+  //   event.preventDefault();
+  //   if (this.state.URI !== '') {
+  //     fetch(`/db/pg?uri=${this.state.URI}`)
+  //       .then((data) => data.json())
+  //       .then((data) => {
+  //         this.addToSearchHistory();
+  //         this.setState({ schema: data, displayCode: true });
+  //         console.log('this.state.schema: ', this.state.schema);
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //       });
+  //   }
+  // }
+  // handleURI(input) {
+  //   this.setState({ URI: input });
+  // }
+  // // for CodeDisplay.jsx/Back button
+  // handleClick(event) {
+  //   event.preventDefault();
+  //   this.setState({ displayCode: false });
+  // }
     return (
       <div className="parent">
         <MainDisplay
@@ -62,11 +64,10 @@ class App extends Component {
           handleURI={this.handleURI}
           displayCode={this.state.displayCode}
           handleClick={this.handleClick}
-          searchHistory={this.state.searchHistory}
+          searchHistory={searchHistory}
         />
       </div>
     );
   }
-}
 
 export default App;
