eb0a77cf205075231fa02c497ece4fb17381f0cc
Sara Chang <sarachang530@gmail.com>
2020-04-18 12:06:41 -0400

    converted react classes into hooks in the input.js file

--- client/Components/Input.js
+++ client/Components/Input.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState , useState} from 'react';
 import HistoryContainer from '../containers/HistoryContainer';
 
-class Input extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      historyOpen: false,
-    };
-    this.toggleHistory = this.toggleHistory.bind(this);
+
+const Input = () => {
+  const [historyOpen, setHistoryOpen] = useState(false);
+
+  const toggleHistory = () => {
+    setHistoryOpen(!historyOpen);
   }
 
-  toggleHistory() {
-    this.setState({ historyOpen: !this.state.historyOpen });
-  }
+  const { searchHistory } = props;
 
-  render() {
-    const { searchHistory } = this.props;
     return (
       <div className="input-form">
         <div className="input-button-row">
           <h1 className="piQL">PiQL 🥒</h1>
           <input
             className="input"
-            value={this.props.URI}
-            onChange={(e) => this.props.handleURI(e.target.value)}
+            value={props.URI}
+            onChange={(e) => props.handleURI(e.target.value)}
           />
           <button
             id="submit-uri"
             className="main-btn"
-            onClick={(e) => this.props.handleInput(e)}
+            onClick={(e) => props.handleInput(e)}
           >
             Submit
           </button>
           <p
             className="toggle-history-text"
-            onClick={() => this.toggleHistory()}
+            onClick={() => toggleHistory()}
           >
             View Past Searches
           </p>
-          {this.state.historyOpen && (
+          {historyOpen && (
             <HistoryContainer searchHistory={searchHistory} />
           )}
         </div>
@@ -48,3 +43,52 @@ class Input extends Component {
 }
 
 export default Input;
+
+
+// class Input extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       historyOpen: false,
+//     };
+//     this.toggleHistory = this.toggleHistory.bind(this);
+//   }
+
+// toggleHistory() {
+//   this.setState({ historyOpen: !this.state.historyOpen });
+// }
+
+
+
+// render() {
+//   const { searchHistory } = this.props;
+//   return (
+//     <div className="input-form">
+//       <div className="input-button-row">
+//         <h1 className="piQL">PiQL 🥒</h1>
+//         <input
+//           className="input"
+//           value={this.props.URI}
+//           onChange={(e) => this.props.handleURI(e.target.value)}
+//         />
+//         <button
+//           id="submit-uri"
+//           className="main-btn"
+//           onClick={(e) => this.props.handleInput(e)}
+//         >
+//           Submit
+//         </button>
+//         <p
+//           className="toggle-history-text"
+//           onClick={() => this.toggleHistory()}
+//         >
+//           View Past Searches
+//         </p>
+//         {this.state.historyOpen && (
+//           <HistoryContainer searchHistory={searchHistory} />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+// }
