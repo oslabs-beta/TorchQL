14ecab5d63d9cff47644d8441feff120575ed7f4
Eric Tang <jivesushi@yahoo.com>
2020-04-22 23:15:07 -0400

    refactor mongoController

--- server/SDL-definedSchemas/controllers-SDL/mongoController.js
+++ server/SDL-definedSchemas/controllers-SDL/mongoController.js
@@ -1,17 +1,20 @@
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 const mongoController = {};
 
 
 mongoController.getCollections = (req, res, next) => {
-	var uri = req.query.uri;
-	mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-
-	const connection = mongoose.connection;
-	
-	connection.once("open", function() {
-		console.log("MongoDB database connection established successfully");
-		console.log(uri);
+	var uri = 'mongodb://admin1:admin1@ds055485.mlab.com:55485/datacenter';
+	console.log(uri, req.query.uri)
+	MongoClient.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
+		console.log("MongoDB connection established successfully");
+		db = client.db('datacenter');
+		db.listCollections().toArray((err, collections) => {
+			console.log('collections: ', collections);
+			client.close();
+			return next();
+		})
 	});
-	return next();
+	
 }
 module.exports = mongoController;
