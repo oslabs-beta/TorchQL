e1d116537d14fe79d756178d80707d921b42ef76
Eric Tang <jivesushi@yahoo.com>
2020-04-23 17:30:36 -0400

    successful connects to MySQL db and retrieves column information from one table using mySQLController middleware

--- server/SDL-definedSchemas/controllers-SDL/mySQLController.js
+++ server/SDL-definedSchemas/controllers-SDL/mySQLController.js
@@ -1,12 +1,40 @@
 const mysql = require('mysql');
 const fs = require('fs');
-const pgQuery = fs.readFileSync('server/queries/tableData.sql', 'utf8');
 const mySQLController = {};
 
 mySQLController.getTables = (req, res, next) => {
-  // const db = mysql.createPool(req.query.uri);
-  res.locals.mysqltables = "mySQLController.getMySQLTables"
-  return next();
+  const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'eric',
+    password : '123456',
+    database : 'acme'
+  })
+  db.connect(err => {
+    if (err) {
+      console.error('An error occurred while connecting to the DB')
+      throw err
+    }
+    console.log('connection successful')
+    const sql = 'SHOW COLUMNS FROM users';
+    const pgQuery = fs.readFileSync('server/queries/tableData.sql', 'utf8');
+    // db.query(sql)
+    // .then((data) => {
+    //   res.locals.mysqltables = data.rows[0].tables;
+    //   return next();
+    // })
+    // .catch((err) =>
+    //   next({
+    //     log: 'There was a problem making database query',
+    //     status: 500,
+    //     message: { err },
+    //   })
+    // );
+    db.query(sql, (err, data) => {
+      if(err) throw err;
+      res.locals.mysqltables = data;
+      return next();
+    });
+  })
 }
 
 module.exports = mySQLController;
