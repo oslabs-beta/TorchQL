728b6edbc42977ac54562c5ec040959983add61e
Zac H <zac.haluza@gmail.com>
2020-04-23 10:44:28 -0400

    got rid of leftover props from earlier typescript demo

--- client/index.tsx
+++ client/index.tsx
@@ -3,7 +3,4 @@ import { render } from 'react-dom';
 
 import App from './App';
 
-render(
-  <App compiler="TypeScript" framework="React" />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
