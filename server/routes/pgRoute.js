1f025916f9ee184edb85cfe7aca2b12bd22e0517
Eric Tang <jivesushi@yahoo.com>
2020-04-23 20:05:58 -0400

    set up separate file for routes for returning MySQL schema

--- server/routes/pgRoute.js
+++ server/routes/pgRoute.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const pgController = require('../SDL-definedSchemas/controllers/pgController');
 const pgProgController = require('../programmatically-definedSchemas/controllers-prog/pg-progController');
-const mySQLController = require('../SDL-definedSchemas/controllers-SDL/mySQLController');
 
 router.get(
   '/pg-sdl',
@@ -24,15 +23,6 @@ router.get('/pg-prog',
   (req, res) => {
     console.log(res.locals.mutations)
     res.status(200).json(res.locals.combine);
-  }
-);
-
-router.get(
-  '/mysql-sdl',
-  mySQLController.getTables,
-  (req, res) => {
-    console.log(res.locals.mysqltables)
-    res.status(200).json(res.locals.mysqltables);
   }
 );
 
