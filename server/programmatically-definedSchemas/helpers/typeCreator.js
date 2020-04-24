2edae2a80151030122d466313454152b51839617
Eric Tang <jivesushi@yahoo.com>
2020-04-24 15:07:22 -0400

    create new file with function for generating types in programmaticschema format

---
+++ server/programmatically-definedSchemas/helpers/typeCreator.js
@@ -0,0 +1,18 @@
+const {
+  storeIndexedColumns,
+  getPrimaryKeyType,
+} = require('../helpers/helperFunctions');
+
+function generateTypes(data) {
+  const allTypes = [];
+  const tables = Object.keys(data);
+  for (const tableName of tables) {
+    const { primaryKey, columns } = data[tableName];
+    const valueObj = storeIndexedColumns(columns, primaryKey);
+    const primaryKeyType = getPrimaryKeyType(primaryKey, columns);
+    // need to add code to skip columns with only primary and foreign keys?
+    allTypes.push(createType(tableName, valueObj, columns));
+  }
+  return allTypes;
+}
+
