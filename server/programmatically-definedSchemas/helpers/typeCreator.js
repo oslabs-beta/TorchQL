cceaed715e510ef8392017383679b53ca7bef287
Eric Tang <jivesushi@yahoo.com>
2020-04-24 21:36:43 -0400

    remove valueObj, primaryKeyType declarations from generateTypes

--- server/programmatically-definedSchemas/helpers/typeCreator.js
+++ server/programmatically-definedSchemas/helpers/typeCreator.js
@@ -8,10 +8,6 @@ function generateTypes(data) {
   const allTypes = [];
   const tables = Object.keys(data);
   for (const tableName of tables) {
-    const { primaryKey, columns } = data[tableName];
-    const valueObj = storeIndexedColumns(columns, primaryKey);
-    const primaryKeyType = getPrimaryKeyType(primaryKey, columns);
-    // need to add code to skip columns with only primary and foreign keys?
     allTypes.push(TypeGenerator.createTypes(tableName, data));
   }
   return allTypes;
