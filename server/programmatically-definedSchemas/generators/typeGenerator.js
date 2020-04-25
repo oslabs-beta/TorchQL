d26eb815264c3e02ff3fab8fa25ca796cc346713
Eric Tang <jivesushi@yahoo.com>
2020-04-24 21:25:18 -0400

    import helper functions into typeGenerator file

---
+++ server/programmatically-definedSchemas/generators/typeGenerator.js
@@ -0,0 +1,4 @@
+const { singular } = require('pluralize');
+const { capitalize } = require('../../SDL-definedSchemas/helpers/helperFunctions');
+const { getDataType, getValsAndTypes } = require('../helpers/helperFunctions');
+const { toCamelCase, toPascalCase } = require('../../SDL-definedSchemas/helpers/helperFunctions');
