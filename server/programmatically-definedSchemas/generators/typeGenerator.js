ed3c666f7120190ff529b6ed644649c2dc117bc0
Eric Tang <jivesushi@yahoo.com>
2020-04-24 21:26:33 -0400

    added createTypes method to TypeGenerator object in typeGenerator file

--- server/programmatically-definedSchemas/generators/typeGenerator.js
+++ server/programmatically-definedSchemas/generators/typeGenerator.js
@@ -2,3 +2,18 @@ const { singular } = require('pluralize');
 const { capitalize } = require('../../SDL-definedSchemas/helpers/helperFunctions');
 const { getDataType, getValsAndTypes } = require('../helpers/helperFunctions');
 const { toCamelCase, toPascalCase } = require('../../SDL-definedSchemas/helpers/helperFunctions');
+const TypeGenerator = {};
+
+TypeGenerator.createTypes = function createTypes(tableName, tables) {
+  const { primaryKey, foreignKeys, columns } = tables[tableName];
+  if (foreignKeys === null || Object.keys(columns).length !== Object.keys(foreignKeys).length + 1) {
+    return `const ${capitalize(singular(tableName))}Type = new GraphQLObjectType({\n`
+      + `  name: '${capitalize(singular(tableName))}',\n`
+      + `  fields: () => ({\n`
+      + `    ${toCamelCase(primaryKey)}: { type: GraphQLString },`
+      + this._columns(primaryKey, foreignKeys, columns)
+      // + this._getRelationships(tableName, tables)
+      + '\n  }),\n});\n';
+  }
+  return '';
+};
