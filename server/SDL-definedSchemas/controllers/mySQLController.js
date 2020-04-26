6470bf70d63a1b857a37bbe6b199c9940fe3d986
Zac H <zac.haluza@gmail.com>
2020-04-26 09:12:42 -0400

    replaced mysql package with mysql2 to cover newer and older dbs, also used more expansive query to receive data on all db tables

--- server/SDL-definedSchemas/controllers/mySQLController.js
+++ server/SDL-definedSchemas/controllers/mySQLController.js
@@ -1,34 +1,34 @@
-const mysql = require('mysql');
-const fs = require('fs');
+const mysql = require('mysql2');
 const mySQLController = {};
 
+// This method is suitable for older SQL databases
 mySQLController.getTables = (req, res, next) => {
-  const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'eric',
-    password : '123456',
-    database : 'acme'
-  })
-  db.connect(err => {
-    if (err) {
-      console.error('An error occurred while connecting to the DB')
-      throw err
+  const config = {
+    host: 'localhost',
+    user: 'zac',
+    password: '123456',
+    database: 'acme',
+  };
+  const connection = mysql.createConnection(config);
+  connection.query(
+    `
+SELECT * FROM information_schema.columns
+WHERE TABLE_SCHEMA = '${config.database}'
+`,
+    (err, results, fields) => {
+      if (err) {
+        return next({
+          log: 'There was a problem making MySQL database query',
+          status: 500,
+          message: { err },
+        });
+      }
+      console.log(results);
+      res.locals.tables = results;
+      res.locals.fields = fields;
+      return next();
     }
-    console.log('connection successful')
-  });
-  const sql = `SHOW fields from users`;
-  // const mySQLQuery = fs.readFileSync('server/queries/mySQLtableData.sql', 'utf8');
-  db.query(sql, (err, data) => {
-    if (err) {
-      return next({
-              log: 'There was a problem making MySQL database query',
-              status: 500,
-              message: { err },
-        })
-    }
-    res.locals.mysqltables = data;
-    return next();
-  });
-}
+  );
+};
 
 module.exports = mySQLController;
