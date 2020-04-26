bed4dbe60c50dc39e8d45d33f8a6a2938ef5a4a4
Zac H <zac.haluza@gmail.com>
2020-04-26 09:11:34 -0400

    changed res.locals var name

--- server/routes/mySQLRoute.js
+++ server/routes/mySQLRoute.js
@@ -1,13 +1,9 @@
 const router = require('express').Router();
 const mySQLController = require('../SDL-definedSchemas/controllers/mySQLController');
 
-router.get(
-  '/sdl',
-  mySQLController.getTables,
-  (req, res) => {
-    console.log(res.locals.mysqltables)
-    res.status(200).json(res.locals.mysqltables);
-  }
-);
+router.get('/sdl', mySQLController.getTables, (req, res) => {
+  console.log(res.locals.tables);
+  res.status(200).json(res.locals.tables);
+});
 
 module.exports = router;
