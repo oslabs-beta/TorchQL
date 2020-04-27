50a35fca7e70b3e955cb8d32953c19cf01dc63c1
Eric Tang <jivesushi@yahoo.com>
2020-04-26 22:56:56 -0400

    refactor mutationCreator with different parameters for deleteMutation

--- server/programmatically-definedSchemas/helpers/mutationCreator.js
+++ server/programmatically-definedSchemas/helpers/mutationCreator.js
@@ -1,7 +1,6 @@
 const MutationGenerator = require('../generators/mutationGenerator');
 const {
   storeIndexedColumns,
-  getPrimaryKeyType,
 } = require('../helpers/helperFunctions');
 
 // Outputs entire mutation object as a string
@@ -11,13 +10,11 @@ function generateMutations(data) {
   for (const tableName of tables) {
     const { primaryKey, columns } = data[tableName];
     const { foreignKeys } = data[tableName];
-    const valueObj = storeIndexedColumns(columns, primaryKey);
-    const primaryKeyType = getPrimaryKeyType(primaryKey, columns);
     // necessary to skip columns with only primary and foreign keys?
     // allMutations.push(createMutation(tableName, valueObj, columns));
     allMutations.push(createMutation(tableName, primaryKey, foreignKeys, columns));
     allMutations.push(updateMutation(tableName, primaryKey, foreignKeys, columns));
-    allMutations.push(deleteMutation(tableName, primaryKey, primaryKeyType));
+    allMutations.push(deleteMutation(tableName, primaryKey, columns));
   }
   return allMutations;
 }
