0e70ca20c72657cc1c98de00dae08d580117d6a2
Eric Tang <jivesushi@yahoo.com>
2020-04-27 22:25:22 -0400

    fixed formatting of queries in QueryGenerator

--- server/programmatically-definedSchemas/generators/queryGenerator.js
+++ server/programmatically-definedSchemas/generators/queryGenerator.js
@@ -6,8 +6,8 @@ const QueryGenerator = {};
 // RootQuery output to get all tables
 QueryGenerator.allColumns = table => {
   const singleName = singular(table);
-    return `${toCamelCase(table)}: {\n`
-    +`    type: ${toPascalCase(singleName)}Type,\n`
+    return `   ${toCamelCase(table)}: {\n`
+    +`      type: ${toPascalCase(singleName)}Type,\n`
     +`      resolve() { \n`
     +`        try { \n`
     +`          const query = \`SELECT * FROM ${table}\`\n`
@@ -24,22 +24,22 @@ QueryGenerator.allColumns = table => {
 // RootQuery output to get single table by id
 QueryGenerator.column = (table, primaryKey) => {
   const singleName = singular(table);
-    return `${toCamelCase(singleName)}ById: {\n`
-    +`    type: ${toPascalCase(singleName)}Type,\n`
-    +`    args: {\n`
-    +`      id: { type: GraphQL[placeholder] },\n` // <-- need to replace placer holder with string/num/id/list
+    return `   ${toCamelCase(singleName)}ById: {\n`
+    +`      type: ${toPascalCase(singleName)}Type,\n`
+    +`      args: {\n`
+    +`        id: { type: GraphQL[placeholder] },\n` // <-- need to replace placer holder with string/num/id/list
+    +`      },\n`
+    +`      resolver(parent, args) {\n`
+    +`        try {\n`
+    +`          const query = 'SELECT * FROM ${table}\n`
+    +`          WHERE ${primaryKey} = $1';\n`
+    +`          const values = [args.${primaryKey}];\n`
+    +`          return db.query(query, values).then((res) => res.rows[0]);\n`
+    +`        } catch (err) {\n`
+    +`          throw new Error(err);\n`
+    +`        }\n`
+    +`      },\n`
     +`    },\n`
-    +`    resolver(parent, args) {\n`
-    +`      try {\n`
-    +`        const query = 'SELECT * FROM ${table}\n`
-    +`        WHERE ${primaryKey} = $1';\n`
-    +`        const values = [args.${primaryKey}];\n`
-    +`        return db.query(query, values).then((res) => res.rows[0]);\n`
-    +`      } catch (err) {\n`
-    +`        throw new Error(err);\n`
-    +`      }\n`
-    +`    },\n`
-    +`  }\n`
 };
 
 module.exports = QueryGenerator;
