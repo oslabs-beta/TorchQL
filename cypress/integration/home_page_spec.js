52a5e7450b6a72b38f682fec3eb0509b520f1b84
Sara Chang <sarachang530@gmail.com>
2020-04-29 12:12:34 -0400

    implemented cypress tests for buttons

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -8,6 +8,16 @@ describe('The Home Page', () => {
       .should('have.class', 'input')
   });
 
+  // it('has a title', () => {
+  //   cy.contains('TORCHQL')
+  //   expect(2).to.equal(2)
+  // })
+  it('checks for button', () => {
+    cy.get('button')
+      .click()
+      .should('have.class', 'main-btn')
+  })
+
   it('accepts input', () => {
     const typedText = 'Test Input 123'
     cy.get('.input')
