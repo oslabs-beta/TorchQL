bf8c75a935cb5a2900e0609f917584ec14577451
Eric Tang <jivesushi@yahoo.com>
2020-04-29 18:11:52 -0400

    added middleware for creating programmatic schema in db/mySQL/prog route in mySQLRoute

--- server/routes/mySQLRoute.js
+++ server/routes/mySQLRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const mySQLController = require('../SDL-definedSchemas/controllers/mySQLController');
 const pgController = require('../SDL-definedSchemas/controllers/pgController');
+const pgProgController = require('../programmatically-definedSchemas/controllers-prog/pg-progController');
 
 router.get('/sdl', 
   mySQLController.getTables, 
@@ -8,6 +9,22 @@ router.get('/sdl',
   (req, res) => {
     console.log(res.locals.SDLSchema);
     res.status(200).json(res.locals.SDLSchema);
+  }
+);
+
+router.get('/prog',
+  mySQLController.getTables, 
+  pgProgController.generateCustomTypes,
+  pgProgController.assembleCustomTypes,
+  pgProgController.generateQuery,
+  pgProgController.formatQueries,
+  pgProgController.generateMutations,
+  pgProgController.assembleMutations,
+  pgProgController.formatMutations,
+  pgProgController.assembleProgSchema,
+  (req, res) => {
+    console.log(res.locals.progSchema);
+    res.status(200).json(res.locals.progSchema);
   }
 );
 
