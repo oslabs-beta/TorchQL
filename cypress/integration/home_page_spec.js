99f83f0388822753d31d1ce9c59d2b14b0bce6e0
Sara Chang <sarachang530@gmail.com>
2020-04-29 15:30:33 -0400

    added postgreSQL test

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -8,20 +8,27 @@ describe('The Home Page', () => {
       .should('have.class', 'input')
   });
 
-  // it('has a title', () => {
-  //   cy.contains('TORCHQL')
-  //   expect(2).to.equal(2)
-  // })
-  it('checks for button', () => {
-    cy.get('button')
-      .click()
-      .should('have.class', 'main-btn')
+  it('has a title', () => {
+    cy.get('.header')
+    cy.contains('TorchQL')
+    expect(2).to.equal(2)
   })
 
   it('accepts input', () => {
     const typedText = 'Test Input 123'
     cy.get('.input')
       .type(typedText)
-      .should('have.value', typedText);
+      .should('have.value', typedText)
+      .click()
   });
+
+  it('mySQL opens div', () => {
+    cy.get('.parent > :nth-child(1) > :nth-child(3)')
+      .click()
+  })
+
+  it('postgreSQL fetch request', () => {
+    cy.request('/db/pg/prog?uri=postgres://zybzettu:rqV_nuf1026KuTDybUHYNDKRRtfGA2a-@raja.db.elephantsql.com:5432/zybzettu')
+    .its('body')
+  })
 });
