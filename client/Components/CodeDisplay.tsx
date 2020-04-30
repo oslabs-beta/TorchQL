e72a355c371b8a89820f84e5a8037f61b881050d
Zac H <zac.haluza@gmail.com>
2020-04-30 08:41:52 -0400

    added fetch requests to update placeholder file upon mount and download when button is clicked

--- client/Components/CodeDisplay.tsx
+++ client/Components/CodeDisplay.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import useScript from '../hooks/useScript';
+import React, { useEffect } from 'react';
 import { UnControlled as CodeMirror } from '../../node_modules/react-codemirror2';
 import '../../node_modules/codemirror/mode/javascript/javascript';
 import '../../node_modules/codemirror/lib/codemirror.css';
@@ -8,13 +7,32 @@ import '../../node_modules/codemirror/theme/dracula.css';
 interface Props {
   schema: string;
   handleClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  saveToFile: () => void;
+  handleDownload: () => void;
 }
 
 export const CodeDisplay: React.FC<Props> = ({ schema, handleClick }) => {
-  useScript('../client/fileSave.js');
+  // For use with Electron
+  // useScript('../client/fileSave.js');
   const invisStyle = {
     display: 'none',
   };
+
+  useEffect(() => {
+    const data = { schema };
+    fetch('/download', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => console.log('file updated'));
+  }, []);
+
+  const handleDownload = () => {
+    window.open('/download/getfile');
+  };
+
   return (
     <div id="codemirror-div">
       <CodeMirror
@@ -30,7 +48,11 @@ export const CodeDisplay: React.FC<Props> = ({ schema, handleClick }) => {
       <button className="main-btn" onClick={(e) => handleClick(e)}>
         Back
       </button>
-      <button className="main-btn" id="save-file">
+      <button
+        className="main-btn"
+        id="save-file"
+        onClick={() => handleDownload()}
+      >
         Save File
       </button>
       <p id="invisible" style={invisStyle}>
