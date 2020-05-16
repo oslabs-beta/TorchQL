165cc94a7260ecb52b77c55a85edb7451447e0df
Eric Tang <jivesushi@yahoo.com>
2020-05-16 19:48:59 -0400

    refactor console.log message shown after npm run start

--- client/templateFunctions/serverCreator.js
+++ client/templateFunctions/serverCreator.js
@@ -17,7 +17,7 @@ function serverCreator() {
   );
 
   app.listen(PORT, () => {
-    console.log("console.log('Welcome to TorchQL! To query your database using your new schemas, please go to http://localhost:3000/graphql');");
+    console.log('Welcome to TorchQL! To query your database using your new schemas, please go to http://localhost:3000/graphql');
   });
   `;
   return fileContent;
