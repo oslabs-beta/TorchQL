585ef0fab97b9d5cbc9e1aa06c26086384004888
Eric Tang <jivesushi@yahoo.com>
2021-02-20 22:14:36 -0500

    refactor maindisplay to conditionally render  components

--- client/components/MainDisplay.tsx
+++ client/components/MainDisplay.tsx
@@ -1,57 +1,21 @@
-import React, { useState, useContext } from 'react';
-import { Input } from './Input';
+import React, { useContext } from 'react';
+import InputContainer from '../containers/InputContainer';
 import { CodeDisplay } from './CodeDisplay';
-import { MySQL } from './MySQL';
 const { UserContext } = require("../context/UserContext");
 
-const MainDisplay: React.FC = (props) => {
+const MainDisplay: React.FC = () => {
   const { displayCode } = useContext(UserContext);
-  const [displayStatus, setDisplayStatus] = useState<string>('postgresql');
+
   return (
     <div>
-      <button
-        className="main-btn"
-        onClick={() => {
-          setDisplayStatus('postgresql');
-        }}
-      >
-        PostgreSQL
-      </button>
-      <button
-        className="main-btn"
-        onClick={() => {
-          setDisplayStatus('mysql');
-        }}
-      >
-        MySQL
-      </button>
-      {displayStatus === 'postgresql' && (
-        <div className="container">
-          {displayCode ? (
+      {!displayCode ? (
+           <div className="container">
+              <InputContainer />
+            </div>
+      ) : (
             <div className="container">
               <CodeDisplay />
-              <Input />
             </div>
-          ) : (
-            <div className="container">
-              <Input />
-            </div>
-          )}
-        </div>
-      )}
-      {displayStatus === 'mysql' && (
-        <div className="container">
-          {displayCode ? (
-            <div className="container">
-              <CodeDisplay />
-              <MySQL />
-            </div>
-          ) : (
-            <div className="container">
-              <MySQL />
-            </div>
-          )}
-        </div>
       )}
     </div>
   );
