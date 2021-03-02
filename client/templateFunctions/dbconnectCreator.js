83c6d00529b703b466fc524030dd57a6634ab4b9
Eric Tang <jivesushi@yahoo.com>
2021-03-01 22:25:24 -0500

    change spacing dbconnectcreator

--- client/templateFunctions/dbconnectCreator.js
+++ client/templateFunctions/dbconnectCreator.js
@@ -1,17 +1,17 @@
 function dbconnectCreator(uri) {
-  const fileContent = `
-    const { Pool } = require('pg');
-    const PG_URI = '${uri}';
-    const pool = new Pool({ connectionString: PG_URI });
+  const fileContent =
+  `const { Pool } = require('pg');
+const PG_URI = '${uri}';
+const pool = new Pool({ connectionString: PG_URI });
 
-    module.exports = {
-      query: (text, params, callback) => {
-        console.log('Executed query:', text);
-        return pool.query(text, params, callback);
-      },
-    };
-    `;
-  return fileContent;
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('Executed query:', text);
+    return pool.query(text, params, callback);
+  },
+};
+`;
+return fileContent;
 }
 
 module.exports = {
