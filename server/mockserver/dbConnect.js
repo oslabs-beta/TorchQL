efae3ef4ad11f113c60f81d31dde23e6c3116259
Eric Tang <jivesushi@yahoo.com>
2021-03-01 21:57:30 -0500

    add default dbConnect file for playground

---
+++ server/mockserver/dbConnect.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+const PG_URI = 'postgres://imvgunqg:NZ7GfBlp74-04PTho1XetRNgaOPgTDXi@drona.db.elephantsql.com:5432/imvgunqg';
+const pool = new Pool({ connectionString: PG_URI });
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('Executed query:', text);
+    return pool.query(text, params, callback);
+  },
+};
+    
